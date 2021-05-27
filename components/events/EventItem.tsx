cf47b359c716405a6b0e82ee972bf911d76877b5
nolbuzanis <nolanbuzanis@gmail.com>
2021-05-27 13:43:03 -0400

    Added icons to event cards

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,4 +1,7 @@
 import Button from '../ui/Button';
+import DateIcon from '../icons/DateIcon';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
+import AddressIcon from '../icons/AddressIcon';
 
 import classes from './EventItem.module.css';
 
@@ -19,14 +22,21 @@ const EventItem = ({ id, title, date, location, image }: EventItem) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}> Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
