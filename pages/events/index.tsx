10872bcd6042eea3bf7c07255748c11a49f471ab
nolbuzanis <nolanbuzanis@gmail.com>
2021-05-30 16:37:45 -0400

    Added static pre-rendering for events and home page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
-import { getAllEvents, getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
 import EventsSearch from '../../components/events/EventsSearch';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
 
-const EventsPage = (): JSX.Element => {
-  const [events, setEvents] = useState(getAllEvents());
+interface EventsPageProps {
+  events: EventItem[];
+}
+
+const EventsPage = (props: EventsPageProps): JSX.Element => {
+  const [events, setEvents] = useState(props.events);
 
   const handleSearch = (year: string, month: string): void => {
     const filteredEvents = getFilteredEvents({
@@ -15,12 +21,36 @@ const EventsPage = (): JSX.Element => {
     setEvents(filteredEvents);
   };
 
+  // if (!props.events) return <div>loading...</div>;
+
   return (
     <>
       <EventsSearch onSearch={handleSearch} />
       <EventList items={events} />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const { data } = await axios.get(
+      'https://nextjs-event-app-default-rtdb.firebaseio.com/events.json'
+    );
+
+    const parsedData = [];
+    for (const key in data) {
+      parsedData.push({ id: key, ...data[key] });
+    }
+
+    return {
+      props: {
+        events: parsedData,
+      },
+      revalidate: 30,
+    };
+  } catch (err) {
+    return { notFound: true };
+  }
 };
 
 export default EventsPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,44 @@
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/EventList';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 
-const HomePage = (): JSX.Element => {
-  const featuredEvents = getFeaturedEvents();
+interface HomePageProps {
+  events: EventItem[];
+}
+
+const HomePage = (props: HomePageProps): JSX.Element => {
+  // const featuredEvents = getFeaturedEvents();
+
+  if (!props.events) return <div>loading...</div>;
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const { data } = await axios.get(
+      'https://nextjs-event-app-default-rtdb.firebaseio.com/events.json'
+    );
+
+    const parsedData = [];
+    for (const key in data) {
+      if (data[key].isFeatured) parsedData.push({ id: key, ...data[key] });
+    }
+
+    return {
+      props: {
+        events: parsedData,
+      },
+      revalidate: 30,
+    };
+  } catch (err) {
+    return { notFound: true };
+  }
 };
 
 export default HomePage;
